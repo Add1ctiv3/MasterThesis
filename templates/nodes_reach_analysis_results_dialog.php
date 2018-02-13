f89877841e547641e9122ea97e3c37b21ec1c1da
Add1ctiv3 <Add1ctiv3@users.noreply.github.com>
2018-02-13 14:38:21 +0200

    Add files via upload

---
+++ templates/nodes_reach_analysis_results_dialog.php
@@ -0,0 +1,18 @@
+<?php
+
+return '
+
+<div id="nodes-reach-analysis-results-panel" class="hidden">
+    
+    <table id="nodes-reach-analysis-results-table">
+        
+        <thead>
+            <th>Number</th>
+            <th>Measure</th>
+        </thead>
+
+    </table>
+
+</div>';
+
+?>
